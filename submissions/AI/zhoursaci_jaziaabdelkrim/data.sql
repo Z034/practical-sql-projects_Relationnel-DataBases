92a84b058831ce1ba39ad1f2832f83e7781e6ec7
Zhour <sacizhour.04@gmail.com>
2026-02-15 09:37:20 +0100

    Fix typo in column name for grades insert statement

--- submissions/AI/zhoursaci_jaziaabdelkrim/data.sql
+++ submissions/AI/zhoursaci_jaziaabdelkrim/data.sql
@@ -64,7 +64,7 @@ INSERT INTO enrollements (student_id, course_id, enrollement_date, academic_year
 
 
 --grades
-INSERT INTO grades (enrollement_id, evaluation_type, grade, coefficient, evaluation_date, comments) VALUES
+INSERT INTO grades (enrollment_id, evaluation_type, grade, coefficient, evaluation_date, comments) VALUES
   (1, 'Assignment', 16, 4, '25-1-2026'),
   (2, 'Assignment', 13, 5, '17-1-2026'),
   (3, 'Assignment', 18, 6, '07-1-2026'),
